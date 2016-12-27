5d88427d2c6f70cbb2deb762df760ce371ec3820
Pittayarat <pittayarat.sae@gmail.com>
2016-12-27 18:40:46 +0700

    save Table checkin, tip, user, photo

--- initialFQ.py
+++ initialFQ.py
@@ -8,6 +8,7 @@ from pandas.io.json import json_normalize
 import dateutil.parser as date
 import json
 import os.path as path
+import datetime
 
 spark = SparkSession\
     .builder\
@@ -22,53 +23,6 @@ with open('fqVenue.json') as json_data:
 
     parquetFile = spark.read.parquet("FQ_VENUE.parquet")
     parquetFile.show()
-    # parquetFile.createOrReplaceTempView("parquetFile")
-    # x = spark.sql("SELECT * FROM parquetFile")
-    # x.show()
 
-# # for tweet
-# if not path.exists("TW_TWEET.parquet"):
-#     with open('tweet.json') as json_data:
-#         tweetsJSON = json.load(json_data)
-#         for index in range(0, len(tweetsJSON)):
-#             tweetsJSON[index]['created_at'] = tweetsJSON[index]['created_at']['$date']
-#             del tweetsJSON[index]['_id']
+    print(datetime.datetime.fromtimestamp(1423633830).isoformat())
 
-#         # rawTweets = []
-#         # for tweet in tweetsJSON:
-#         #     normalizedTweet = json_normalize(tweet)
-#         #     map(lambda column: normalizedTweet.rename(columns = {column: ''.join(map(lambda t: t.replace(".", "_"), list(column)))}, inplace = True) ,normalizedTweet.columns)
-#         #     rawTweets.append(normalizedTweet.to_json())
-#         # rawTweetRDD = sc.parallelize(rawTweets)
-#         # rawTweetDF = spark.read.json(rawTweetRDD)
-#         # rawTweetDF.write.parquet("rawTweet.parquet")
-
-#         tweets = []
-#         users = []
-#         places = []
-#         for tweet in tweetsJSON:
-#             queryDF = spark.read.parquet("QUERY.parquet")
-#             keywords = queryDF.select(queryDF.id, queryDF.keyword).collect()
-#             for keyword in keywords:
-#                 if keyword['keyword'] in tweet['text']:
-#                     tweets.append(TwitterRepository.selectTweetCol(tweet, keyword['id']))
-#             users.append(TwitterRepository.selectUserCol(tweet['user']))
-#         tweetRDD = sc.parallelize(tweets)
-#         tweetDF = spark.createDataFrame(tweetRDD)
-#         tweetDF.printSchema()
-#         tweetDF.write.parquet("TW_TWEET.parquet")
-#         userRDD = sc.parallelize(users)
-#         userDF = spark.createDataFrame(userRDD)
-#         if not path.exists("TW_USER.parquet"):
-#             userDF.write.parquet('TW_USER.parquet')
-
-# #for query
-# if not path.exists("QUERY.parquet"):
-#     with open('tweetQuery.json') as json_data:
-#         queryJson = json.load(json_data)
-#         queries = []
-#         for query in queryJson['queries']:
-#             queries.append(SocialDataRepository.createQuerySchema(query))
-#         queryRDD = sc.parallelize(queries)
-#         queryDF = spark.createDataFrame(queryRDD)
-#         queryDF.write.parquet("QUERY.parquet")
--- repository/FoursquareRepository.py
+++ repository/FoursquareRepository.py
@@ -3,9 +3,10 @@ from pyspark import SparkContext
 from pyspark.sql import *
 from pandas.io.json import json_normalize
 from geopy.distance import great_circle
-import dateutil.parser as date
 import uuid
 import os.path as path
+import datetime
+import dateutil.parser as date
 
 
 spark = SparkSession\
@@ -15,22 +16,21 @@ spark = SparkSession\
 
 sc = spark.sparkContext
 
+def writeParquet(parquetFile,rowArr):
+    rowRDD = sc.parallelize(rowArr)
+    rowDF = spark.createDataFrame(rowRDD)
+    rowDF.write.mode("append").parquet(parquetFile)
 
 #FQ_VENUE####################################
 def saveVenue(venue,queryId):
-
     venueParquet = "FQ_VENUE.parquet"
     if path.exists(venueParquet):     
         venueBaseDF = spark.read.parquet(venueParquet)
         existVenue = venueBaseDF.where(venueBaseDF.venueid == venue['id'])
         if existVenue.count() == 0:
-            venueRDD = sc.parallelize([selectVenueCol(venue,queryId)])
-            venueDF = spark.createDataFrame(venueRDD)
-            venueDF.write.mode("append").parquet(venueParquet)
+            writeParquet(venueParquet,[selectVenueCol(venue,queryId)])
     else:
-        venueRDD = sc.parallelize([selectVenueCol(venue,queryId)])
-        venueDF = spark.createDataFrame(venueRDD)
-        venueDF.write.parquet(venueParquet)            
+        writeParquet(venueParquet,[selectVenueCol(venue,queryId)])            
 
 def selectVenueCol(venue,queryId):
     newVenue = {}
@@ -40,3 +40,83 @@ def selectVenueCol(venue,queryId):
     newVenue['category'] = venue['categories'][0]['name']
     return newVenue
 
+#FQ_CHECKIN####################################
+def saveCheckin(checkin,venueId):
+    checkinParquet = "FQ_CHECKIN.parquet"
+    writeParquet(checkinParquet,[selectCheckinCol(checkin,queryId)])             
+
+def selectCheckinCol(checkin,venueId):
+    newCheckin = {}
+    newCheckin['id'] = str(uuid.uuid4())
+    newCheckin['datetime'] = datetime.datetime.now().isoformat() 
+    newCheckin['count'] = checkin['count']
+    newCheckin['venueid'] = venueId
+    return newCheckin
+
+#FQ_TIP####################################
+def saveTip(tips,venueId):
+    tipParquet = "FQ_TIP.parquet"
+    allTips = []
+    if path.exists(tipParquet):     
+        tipBaseDF = spark.read.parquet(tipParquet)
+        for tip in tips['items']:
+            existTip = tipBaseDF.where(tipBaseDF.tipid == tip['id'])
+            if existTip.count() == 0:
+                allTips.append(selectTipCol(tip,venueId))
+        writeParquet(checkinParquet,allTips)     
+    else:
+        for tip in tips['items']:
+            allTips.append(selectTipCol(tip,venueId))
+        writeParquet(tipParquet,allTips)            
+
+def selectTipCol(tip,venueId):
+    newTip = {}
+    newTip['tipid'] = tip['id']
+    newTip['datetime'] =  datetime.datetime.fromtimestamp(tip['createdAt']).isoformat()
+    newTip['message'] = tip['text']
+    newTip['venueid'] = venueId
+    newTip['userid'] = tip['user']['id']
+    return newTip
+
+#FQ_USER####################################
+def saveUser(user):
+    userParquet = "FQ_USER.parquet"
+    if path.exists(userParquet):     
+        userBaseDF = spark.read.parquet(userParquet)
+        existUser = userBaseDF.where(userBaseDF.userid == user['id'])
+        if existUser.count() == 0:
+            writeParquet(userParquet,[selectUserCol(user)])
+    else:
+        writeParquet(userParquet,[selectUserCol(user)])            
+
+def selectUserCol(user):
+    newUser = {}
+    newUser['userid'] = user['id']
+    newUser['name'] = user['firstName']+' '+user['lastName']
+    newUser['gender'] = user['gender']
+    return newUser
+
+#FQ_PHOTO####################################
+def savePhoto(photos,venueId):
+    photoParquet = "FQ_PHOTO.parquet"
+    allPhoto = []
+    if path.exists(photoParquet):     
+        photoBaseDF = spark.read.parquet(photoParquet)
+        for photo in photos['items']:
+            existPhoto = photoBaseDF.where(photoBaseDF.photoid == photo['id'])
+            if existPhoto.count() == 0:
+                allPhoto.append(selectPhotoCol(photo,venueId))
+        writeParquet(photoParquet,allPhoto)     
+    else:
+        for photo in photos['items']:
+            allPhoto.append(selectPhotoCol(photo,venueId))
+        writeParquet(photoParquet,allPhoto)            
+
+def selectPhotoCol(photo,venueId):
+    newPhoto = {}
+    newPhoto['photoid'] = photo['id']
+    newPhoto['datetime'] =  datetime.datetime.fromtimestamp(photo['createdAt']).isoformat()
+    newPhoto['photo'] = photo['prefix']+photo['suffix']
+    newPhoto['venueid'] = venueId
+    newPhoto['userid'] = photo['user']['id']
+    return newTip
